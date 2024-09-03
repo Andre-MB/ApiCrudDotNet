cbf3479b11fa937a6a9246f10c66a511e319293f
Andre-MB <andrezinhogame15@gmail.com>
2024-09-03 00:25:50 -0300

    Referencia Ciclica

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using ApiUdemy.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions
+            .ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
