7a84082d2199097e04ddc142618f5b9c31dfc5fc
Andre-MB <andrezinhogame15@gmail.com>
2024-09-03 17:57:30 -0300

    CategoriaRepository

--- Repositories/CategoriaRepository.cs
+++ Repositories/CategoriaRepository.cs
@@ -1,5 +1,61 @@
-﻿namespace ApiUdemy.Repositories;
+﻿using ApiUdemy.Context;
+using ApiUdemy.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class CategoriaRepository
+namespace ApiUdemy.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
 {
+    private readonly ApiDbContext _context;
+
+    public CategoriaRepository(ApiDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Categoria> GetCategorias()
+    {
+        return _context.Categorias.ToList();
+    }
+
+    public Categoria GetCategoria(int id)
+    {
+        return _context.Categorias.FirstOrDefault(c => c.Id == id);
+    }
+
+    public Categoria Create(Categoria categoria)
+    {
+        if(categoria is null) 
+            throw new ArgumentNullException(nameof(categoria));
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return categoria;
+    }
+
+    public Categoria Update(Categoria categoria)
+    {
+        if (categoria is null)
+            throw new ArgumentNullException(nameof(categoria));
+
+        _context.Entry(categoria).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return categoria;
+    }
+
+    public Categoria Delete(int id)
+    {
+        var categoria = _context.Categorias.Find(id);
+
+        if (categoria is null)
+            throw new ArgumentNullException(nameof(categoria));
+
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+
+        return categoria;
+    }
+
 }
