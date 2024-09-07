044afea3f9511cf94644dc2d17f56b0d49a01560
Andre-MB <andrezinhogame15@gmail.com>
2024-09-06 22:45:09 -0300

    Irepository

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -8,5 +8,6 @@ public interface IRepository<T>
     IEnumerable<T> GetAll();
     T? Get(Expression<Func<T, bool>> predicate);
     T Create(T entity);
-    
+    T Update(T entity);
+    T Delete(T entity);
 }
