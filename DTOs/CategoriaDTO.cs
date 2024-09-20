7aadb4169479bf9f904d81aff35b938198ffd76b
Andre-MB <andrezinhogame15@gmail.com>
2024-09-19 23:42:26 -0300

    compondo CategoriaDTO

--- DTOs/CategoriaDTO.cs
+++ DTOs/CategoriaDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace ApiUdemy.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiUdemy.DTOs;
+
+public class CategoriaDTO
 {
-    public class CategoriaDTO
-    {
-    }
+    public int Id { get; set; }
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
 }
