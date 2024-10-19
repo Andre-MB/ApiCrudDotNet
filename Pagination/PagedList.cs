bdf3c315b295b930e0e7f9742986ae324983c971
Andre-MB <andrezinhogame15@gmail.com>
2024-10-18 23:36:43 -0300

    types the class PagedList

--- Pagination/PagedList.cs
+++ Pagination/PagedList.cs
@@ -1,5 +1,15 @@
 ﻿namespace ApiUdemy.Pagination;
 
-public class PagedList
+public class PagedList<T> : List<T> where T : class
 {
+    public int CurrentPage { get; private set; }
+    public int TotalPages { get; private set; }
+    public int PageSizze { get; private set; }
+    public int TotalCount { get; private set; }
+
+    public bool HasPrecious => CurrentPage > 1;
+
+    public bool HasNex => CurrentPage < TotalPages;
+
+
 }
