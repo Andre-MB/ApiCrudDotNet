9efdba0a10bf50573256f768f4ff8ae13d8bfb5c
Andre-MB <andrezinhogame15@gmail.com>
2024-10-31 22:09:36 -0300

    method PagedList

--- Pagination/PagedList.cs
+++ Pagination/PagedList.cs
@@ -4,12 +4,18 @@ public class PagedList<T> : List<T> where T : class
 {
     public int CurrentPage { get; private set; }
     public int TotalPages { get; private set; }
-    public int PageSizze { get; private set; }
+    public int PageSize { get; private set; }
     public int TotalCount { get; private set; }
 
     public bool HasPreciousss => CurrentPage > 1;
 
     public bool HasNex => CurrentPage < TotalPages;
 
-
+    public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageSize = pageSize;
+        CurrentPage = pageNumber;
+        TotalPages = (int)Math.Ceiling(count/(double)pageSize);
+    }
 }
